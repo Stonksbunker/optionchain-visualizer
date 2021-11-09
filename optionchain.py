e743e8439f5ce902dff1338259f5a666f276af48
yadavnarun <n.y.narun@gmail.com>
2021-11-09 21:53:14 +0530

    updated tabs in dash

--- optionchain.py
+++ optionchain.py
@@ -41,157 +41,182 @@ request_headers = {
     'Pragma': 'no-cache',
     'Cache-Control': 'no-cache',
 }
-
 nse_url = 'https://www.nseindia.com/'
 
 
-def output(scrip):
+def fetch_data(scrip):
+    try:
+        perf = {}
 
-    fno_data_url = 'https://www.nseindia.com/api/quote-derivative?symbol=' + \
-        requests.utils.quote(scrip)
-    resp = requests.get(url=nse_url, headers=request_headers)
-    if resp.ok:
-        req_cookies = dict(
-            nsit=resp.cookies['nsit'], nseappid=resp.cookies['nseappid'], ak_bmsc=resp.cookies['ak_bmsc'])
-        response = requests.get(
-            url=fno_data_url, headers=request_headers, cookies=req_cookies).json()
-        with open('./data/' + today + '-data/fno_data_' + scrip + '.json', 'w') as f:
-            json.dump(response, f)
+        with open('./data/' + today + '-data/fno_data_' + scrip + '.json') as g:
+            perf = json.load(g)
 
-    with open('./data/' + today + '-data/fno_data_' + scrip + '.json') as g:
-        perf = json.load(g)
+        return perf
+    except:
+        fno_data_url = 'https://www.nseindia.com/api/quote-derivative?symbol=' + \
+            requests.utils.quote(scrip)
+        resp = requests.get(url=nse_url, headers=request_headers)
+        if resp.ok:
+            req_cookies = dict(
+                nsit=resp.cookies['nsit'], nseappid=resp.cookies['nseappid'], ak_bmsc=resp.cookies['ak_bmsc'])
+            response = requests.get(
+                url=fno_data_url, headers=request_headers, cookies=req_cookies).json()
+            with open('./data/' + today + '-data/fno_data_' + scrip + '.json', 'w') as f:
+                json.dump(response, f)
+
+        with open('./data/' + today + '-data/fno_data_' + scrip + '.json') as g:
+            perf = json.load(g)
+
+        return perf
+
+
+def expiry(scrip):
+    perf = fetch_data(scrip)
 
     # listing all expiry dates
     dates = list(dict.fromkeys(perf['expiryDates']))
 
     # ther are three differnt expiry options contract
-    for m in range(0, 1):
-        try:
-            expiry = dates[m]
-        except:
-            break
+    return dates[0]
 
-        labels = []
-        maxpain = []
 
-        # listing strikes price in sorted order
-        for i in perf['stocks']:
-            if i['metadata']['expiryDate'] == expiry:
-                labels.append(i['metadata']['strikePrice'])
+def labels(scrip):
+    perf = fetch_data(scrip)
+    exp = expiry(scrip)
 
-        labels = list(dict.fromkeys(labels))
-        labels.sort()
+    labels = []
 
-        # open interest data array maker
-        def oi_count(type, value):
-            oi = []
+    # listing strikes price in sorted order
+    for i in perf['stocks']:
+        if i['metadata']['expiryDate'] == exp:
+            labels.append(i['metadata']['strikePrice'])
 
-            for i in labels:
-                temp = len(oi)
-                for j in perf['stocks']:
-                    if j['metadata']['expiryDate'] == expiry and j['metadata']['optionType'] == type:
-                        if str(j['metadata']['strikePrice']) == str(i):
-                            if value != 'lastPrice':
-                                oi.append(j['marketDeptOrderBook']
-                                          ['tradeInfo'][value])
-                            else:
-                                oi.append(j['metadata'][value])
+    labels = list(dict.fromkeys(labels))
+    labels.sort()
 
-                if temp == len(oi):
-                    oi.append(0)
+    return labels
 
-            return oi
 
-        call = oi_count('Call', 'openInterest')
-        put = oi_count('Put', 'openInterest')
+def data_count(scrip, type, value):
+    oi = []
+    perf = fetch_data(scrip)
+    label = labels(scrip)
+    exp = expiry(scrip)
 
-        change_call = oi_count('Call', 'changeinOpenInterest')
-        change_put = oi_count('Put', 'changeinOpenInterest')
-
-        ltp_call = oi_count('Call', 'lastPrice')
-        ltp_put = oi_count('Put', 'lastPrice')
-
-        underlyingValue = perf['underlyingValue']
-
-        strikePrices = perf['strikePrices']
-        strikePrices = list(dict.fromkeys(strikePrices))
-
-        # finding nearest strike price for futer calculations
-        nearest = []
-        l = 0
-        for i in strikePrices:
-            nearest.append(i - underlyingValue)
-
-        for i in nearest:
-            if i < 0:
-                nearest[l] = nearest[l] * -1
-                l = l + 1
-
-        value = strikePrices[nearest.index(min(nearest))]
+    for i in label:
+        temp = len(oi)
         for j in perf['stocks']:
-            if j['metadata']['expiryDate'] == expiry and j['metadata']['strikePrice'] == value:
-                if j['metadata']['optionType'] == 'Call':
-                    iv_call = j['marketDeptOrderBook']['otherInfo']['impliedVolatility']
-                if j['metadata']['optionType'] == 'Put':
-                    iv_put = j['marketDeptOrderBook']['otherInfo']['impliedVolatility']
+            if j['metadata']['expiryDate'] == exp and j['metadata']['optionType'] == type:
+                if str(j['metadata']['strikePrice']) == str(i):
+                    if value != 'lastPrice':
+                        oi.append(j['marketDeptOrderBook']
+                                  ['tradeInfo'][value])
+                    else:
+                        oi.append(j['metadata'][value])
 
-        prob = []
-        daysexpiry = (datetime.datetime.strptime(expiry, '%d-%b-%Y') -
-                      datetime.datetime.strptime(today, '%Y-%m-%d')).days
+        if temp == len(oi):
+            oi.append(0)
 
-        for i in labels:
-            # STRIKE<VALUE WIN PROB => NORMSDIST(LN(STRIKEPRICE/VALUE)/CALLIV*SQRT(DAYSTOEXPIRATION/365))
-            # normsdist = (1/2pi)^2 * e^(-(z^2)/2)
-            if i < value and i > 0:
-                try:
-                    normsdist = math.log(i/underlyingValue) / \
-                        ((iv_call/100)*math.sqrt(daysexpiry/365))
-                    result = integrate.quad(lambda q: (
-                        (1/math.sqrt(2 * math.pi))*((math.e)**(q**2/-2.0))), -np.inf, normsdist)
-                    prob.append(100 - result[0]*100)
-                except:
-                    prob.append(0)
-            elif i > value and i > 0:
-                try:
-                    normsdist = math.log(i/underlyingValue) / \
-                        ((iv_put/100)*math.sqrt(daysexpiry/365))
-                    result = integrate.quad(lambda q: (
-                        (1/math.sqrt(2 * math.pi))*((math.e)**(q**2/-2.0))), -np.inf, normsdist)
-                    prob.append(result[0]*100)
-                except:
-                    prob.append(0)
-            else:
+    return oi
+
+
+def probability(scrip):
+    nearest = []
+    perf = fetch_data(scrip)
+    underlyingValue = perf['underlyingValue']
+    strikePrices = perf['strikePrices']
+    strikePrices = list(dict.fromkeys(strikePrices))
+
+    label = labels(scrip)
+    exp = expiry(scrip)
+
+    l = 0
+    for i in strikePrices:
+        nearest.append(i - underlyingValue)
+
+    for i in nearest:
+        if i < 0:
+            nearest[l] = nearest[l] * -1
+            l = l + 1
+
+    value = strikePrices[nearest.index(min(nearest))]
+
+    for j in perf['stocks']:
+        if j['metadata']['expiryDate'] == exp and j['metadata']['strikePrice'] == value:
+            if j['metadata']['optionType'] == 'Call':
+                iv_call = j['marketDeptOrderBook']['otherInfo']['impliedVolatility']
+            if j['metadata']['optionType'] == 'Put':
+                iv_put = j['marketDeptOrderBook']['otherInfo']['impliedVolatility']
+
+    prob = []
+    daysexpiry = (datetime.datetime.strptime(exp, '%d-%b-%Y') -
+                  datetime.datetime.strptime(today, '%Y-%m-%d')).days
+
+    for i in label:
+        # STRIKE<VALUE WIN PROB => NORMSDIST(LN(STRIKEPRICE/VALUE)/CALLIV*SQRT(DAYSTOEXPIRATION/365))
+        # normsdist = (1/2pi)^2 * e^(-(z^2)/2)
+        if i < value and i > 0:
+            try:
+                normsdist = math.log(i/underlyingValue) / \
+                    ((iv_call/100)*math.sqrt(daysexpiry/365))
+                result = integrate.quad(lambda q: (
+                    (1/math.sqrt(2 * math.pi))*((math.e)**(q**2/-2.0))), -np.inf, normsdist)
+                prob.append(100 - result[0]*100)
+            except:
                 prob.append(0)
+        elif i > value and i > 0:
+            try:
+                normsdist = math.log(i/underlyingValue) / \
+                    ((iv_put/100)*math.sqrt(daysexpiry/365))
+                result = integrate.quad(lambda q: (
+                    (1/math.sqrt(2 * math.pi))*((math.e)**(q**2/-2.0))), -np.inf, normsdist)
+                prob.append(result[0]*100)
+            except:
+                prob.append(0)
+        else:
+            prob.append(0)
 
-        # finding maxpain
-        for i in labels:
-            max_l = 0
+    return prob
 
-            for k in range(0, labels.index(i) + 1):
-                max_l = max_l + (i - labels[k])*call[k]
 
-            for k in range(labels.index(i), len(labels)):
-                max_l = max_l + (labels[k] - i)*put[k]
+def pcr(scrip):
+    j = 0
+    ratio = []
+    call = data_count(scrip, 'Call', 'openInterest')
+    put = data_count(scrip, 'Put', 'openInterest')
+    label = labels(scrip)
 
-            maxpain.append(max_l)
-
-        minpos = maxpain.index(min(maxpain))
-
-        x = np.arange(len(labels))
-
-        pcr = []
-        j = 0
-        for i in labels:
-            if call[j] == 0:
-                pcr.append(1)
+    for i in label:
+        if call[j] == 0:
+            ratio.append(1)
+        else:
+            if put[j]/call[j] > 10:
+                ratio.append(-1)
             else:
-                if put[j]/call[j] > 10:
-                    pcr.append(-1)
-                else:
-                    pcr.append(put[j]/call[j])
-            j = j + 1
+                ratio.append(put[j]/call[j])
+        j = j + 1
 
-        return [labels, call, put, change_call, change_put, ltp_call, ltp_put, prob, pcr, maxpain, minpos]
+    return ratio
+
+
+def maxpain(scrip):
+
+    call = data_count(scrip, 'Call', 'openInterest')
+    put = data_count(scrip, 'Put', 'openInterest')
+    label = labels(scrip)
+
+    for i in labels:
+        max_l = 0
+
+        for k in range(0, labels.index(i) + 1):
+            max_l = max_l + (i - labels[k])*call[k]
+
+        for k in range(labels.index(i), len(labels)):
+            max_l = max_l + (labels[k] - i)*put[k]
+
+        maxpain.append(max_l)
+
+    minpos = maxpain.index(min(maxpain))
 
 
 app = dash.Dash('Optionchain', external_stylesheets=[
@@ -202,10 +227,6 @@ app = dash.Dash('Optionchain', external_stylesheets=[
 with open('fno.json') as g:
   fno_scrips = json.load(g)
 
-# fno_scrips = []
-# for i in fno_list['data']:
-#   fno_scrips.append(i['symbol'])
- 
 
 app.layout = html.Div([
     dcc.Dropdown(
@@ -218,29 +239,17 @@ app.layout = html.Div([
         dcc.Tab(label='Call & Put', children=[
             dcc.Graph(id='callput')
         ]),
-        dcc.Tab(label='Tab two', children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [1, 4, 1],
-                            'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [1, 2, 3],
-                         'type': 'bar', 'name': u'Montréal'},
-                    ]
-                }
-            )
+        dcc.Tab(label='PCR', children=[
+            dcc.Graph(id="pcr")
         ]),
-        dcc.Tab(label='Tab three', children=[
-            dcc.Graph(
-                figure={
-                    'data': [
-                        {'x': [1, 2, 3], 'y': [2, 4, 3],
-                            'type': 'bar', 'name': 'SF'},
-                        {'x': [1, 2, 3], 'y': [5, 4, 3],
-                         'type': 'bar', 'name': u'Montréal'},
-                    ]
-                }
-            )
+        dcc.Tab(label='Change', children=[
+            dcc.Graph(id="change")
+        ]),
+        dcc.Tab(label='LTP', children=[
+            dcc.Graph(id="ltp")
+        ]),
+        dcc.Tab(label='Probability', children=[
+            dcc.Graph(id="probab")
         ]),
     ])
 ], style={'width': '500'})
@@ -248,22 +257,78 @@ app.layout = html.Div([
 
 @ app.callback(Output('callput', 'figure'), [Input('scrip', 'value')])
 def update_graph(selected_dropdown_value):
-    inp = output(selected_dropdown_value)
     return {
         'data': [{
-            'x': inp[0],
-            'y': inp[1],
+            'x': labels(selected_dropdown_value),
+            'y': data_count(selected_dropdown_value, 'Call', 'openInterest'),
             'type': 'bar', 'name': 'call'
         },
             {
-            'x': inp[0],
-            'y': inp[2],
+            'x': labels(selected_dropdown_value),
+            'y': data_count(selected_dropdown_value, 'Put', 'openInterest'),
             'type': 'bar', 'name': 'put'
+        }
+        ],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+
+@ app.callback(Output('pcr', 'figure'), [Input('scrip', 'value')])
+def update_graph(selected_dropdown_value):
+    return {
+        'data': [{
+            'x': labels(selected_dropdown_value),
+            'y': pcr(selected_dropdown_value),
+            'type': 'scatter', 'name': 'pcr'
+        }],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+
+@ app.callback(Output('change', 'figure'), [Input('scrip', 'value')])
+def update_graph(selected_dropdown_value):
+    return {
+        'data': [{
+            'x': labels(selected_dropdown_value),
+            'y': data_count(selected_dropdown_value, 'Call', 'changeinOpenInterest'),
+            'type': 'bar', 'name': 'call-changeinOpenInterest'
         },
             {
-            'x': inp[0],
-            'y': inp[8],
-            'type': 'scatter', 'name': 'pcr'
+            'x': labels(selected_dropdown_value),
+            'y': data_count(selected_dropdown_value, 'Put', 'changeinOpenInterest'),
+            'type': 'bar', 'name': 'put-changeinOpenInterest'
+        }
+        ],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+
+@ app.callback(Output('ltp', 'figure'), [Input('scrip', 'value')])
+def update_graph(selected_dropdown_value):
+    return {
+        'data': [{
+            'x': labels(selected_dropdown_value),
+            'y': data_count(selected_dropdown_value, 'Call', 'lastPrice'),
+            'type': 'bar', 'name': 'call-lastPrice'
+        },
+            {
+            'x': labels(selected_dropdown_value),
+            'y': data_count(selected_dropdown_value, 'Put', 'lastPrice'),
+            'type': 'bar', 'name': 'put-lastPrice'
+        }
+        ],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+
+@ app.callback(Output('probab', 'figure'), [Input('scrip', 'value')])
+def update_graph(selected_dropdown_value):
+    # inp = output(selected_dropdown_value)
+    return {
+        'data': [{
+            'x': labels(selected_dropdown_value),
+            'y': probability(selected_dropdown_value),
+            'type': 'scatter', 'name': 'probability'
         }
         ],
         'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
